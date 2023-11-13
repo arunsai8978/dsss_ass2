2e3b940b804feec0a26b28ac6a7b1c01e9da9fa1
Arun Sai Thunga <117271751+arunsai8978@users.noreply.github.com>
2023-11-13 22:12:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'random',
+        'unittest'
+        # Add other dependencies here
+    ],
+    # Other metadata like author, author_email, description, etc.
+)
 
